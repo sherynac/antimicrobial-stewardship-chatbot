f55f3637ba5a805c7bd4fbcc7a58efc9e11f874c
MARC JANSEN GALVAN <2230714@slu.edu.ph>
2026-03-17 05:18:54 +0800

    Frontend changes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ophiuchus_logo from './assets/ophiuchus_logo.svg'
+import './App.css'
+
+function App() {
+const navigation=[
+  {}
+]
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
