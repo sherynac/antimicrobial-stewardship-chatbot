be4890985d27712576400d2812c4aeeb5d0d43b2
Emily Sotelo <2230635@slu.edu.ph>
2026-03-16 14:25:11 +0800

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import FAQs from "./pages/FAQs";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +31,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/faqs" element={<FAQs />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
