7a7eb8926ef358e47d84a69d218ab6ad8f51273a
Emily Sotelo <2230635@slu.edu.ph>
2026-04-04 18:43:36 +0800

    -about and faq

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,107 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ophiuchus_logo from './assets/ophiuchus_logo.svg'
+import clear_chat from './assets/clear-chat.svg'
+import menu from './assets/menu.svg'
+import about_icon from './assets/about.svg'
+import chat_icon from './assets/chat.svg'
+import faqs_icon from './assets/FAQs.svg'
+import send_button from './assets/send-button.svg'
+
 import './App.css'
+
+import FAQ from './components/faq.jsx'
+import About from './components/about.jsx'
 
 function App() {
 // const navigation=[
 //   {}
 // ]
+
+// useState('chat') - chat UI ang landing
+  const [activePage, setActivePage] = useState('chat') // 'chat', 'about', 'faqs'
+
   return (
     <>
 
       <div className="navbar">
         <div className="first-row">
           <div className="button-container" id="clear-chat-button">
-            <img src="src\assets\clear-chat.svg" alt="clear-icon" className="icons" />
+            <img src={clear_chat} alt="clear-icon" className="icons" />
             <a href="" id="clear-chat">Clear Chat</a>
           </div>
           <div className="menu-button-container" id="menu-button">
-            <img src="src\assets\menu.svg" alt="menu-icon" className="icons" />
+            <img src={menu} alt="menu-icon" className="icons" />
           </div>
         </div>
-        <div className="button-container-active">
-          <img src="src\assets\chat.svg" alt="chat-icon" className="icons" />
-          <a href="" id="chat">Chat</a>
+        
+        {/* chat */}
+        <div 
+          className={activePage === 'chat' ? "button-container-active" : "button-container"}
+          onClick={() => setActivePage('chat')}
+        >
+          <img src={chat_icon} alt="chat-icon" className="icons" />
+          <div className="nav-title">Chat</div>
         </div>
-        <div className="button-container">
-          <img src="src\assets\about.svg" alt="about-icon" className="icons" />
-          <a href="" id="about">About</a>
+
+        {/* about */}
+        <div 
+          className={activePage === 'about' ? "button-container-active" : "button-container"}
+          onClick={() => setActivePage('about')}
+        >
+          <img src={about_icon} alt="about-icon" className="icons" />
+          <div className="nav-title">About</div>
         </div>
-        <div className="button-container">
-          <img src="src\assets\FAQs.svg" alt="FAQs-icon" className="icons" />
-          <a href="" id="faqs">FAQs</a>
-          </div>
+
+        {/* FAQs */}
+        <div 
+          className={activePage === 'faqs' ? "button-container-active" : "button-container"}
+          onClick={() => setActivePage('faqs')}
+        >
+          <img src={faqs_icon} alt="FAQs-icon" className="icons" />
+          <div className="nav-title">FAQs</div>
+        </div>
       </div>
 
       <div className="main-container">
-        <div className="header">
-          <img src={ophiuchus_logo} className="" alt="Ophiuchus logo" />
-            <div className="title-container">
-              <p className="title">Ophiuchus</p>
-              <p className="sub-title">Ask me anything</p>
-            </div>
-        </div>
 
         <div className="main-content">
-          <div className="main-chat">
-            <div className="message-container">
-              <div className="message-prompt">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+          {activePage === 'chat' && (
+            <>
+
+              <div className="header">
+                <img src={ophiuchus_logo} className="" alt="Ophiuchus logo" />
+                  <div className="title-container">
+                    <p className="title">Ophiuchus</p>
+                    <p className="sub-title">Ask me anything</p>
+                  </div>
               </div>
-            </div>
-            <div className="reply-container">
-              <div className="chatbot-reply">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+
+              <div className="main-chat">
+                <div className="message-container">
+                  <div className="message-prompt">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  </div>
+                </div>
+                <div className="reply-container">
+                  <div className="chatbot-reply">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="message-box">
-            <form action="">
-              <input type="text" id="message" placeholder='Type your message here..'/>
-              <input type="image" src="src\assets\send-button.svg" alt="Submit"/>
-            </form>
-          </div>
+              <div className="message-box">
+                <form action="">
+                  <input type="text" id="message" placeholder='Type your message here..'/>
+                  <input type="image" src={send_button} alt="Submit"/>
+                </form>
+              </div>
+            </>
+          )}
+
+          {activePage === 'about' && <About />}
+          
+          {activePage === 'faqs' && <FAQ />}
+
         </div>
       </div>
       
---
+++ frontend/src/components/faq.jsx
@@ -0,0 +1,58 @@
+import './faq.css';
+import ophiuchus_logo from '../assets/ophiuchus_logo.svg'
+
+function FAQ() {
+
+    const sampleQA = [
+
+        {
+            question: "The standard Lorem Ipsum passage, used since the 1500s",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        },
+        {
+            question: "The standard Lorem Ipsum passage, used since the 1500s",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        },
+        {
+            question: "The standard Lorem Ipsum passage, used since the 1500s",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        },
+        {
+            question: "The standard Lorem Ipsum passage, used since the 1500s",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        },
+        {
+            question: "The standard Lorem Ipsum passage, used since the 1500s",
+            answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        }
+    ];
+
+    return (
+        <>
+            <div className="header">
+                <img src={ophiuchus_logo} className="" alt="Ophiuchus logo" />
+                    <div className="title-container">
+                    <p className="title">FAQs</p>
+                    <p className="sub-title">Find answers to common questions</p>
+                    </div>
+            </div>
+
+            <div className="faq-container">
+                <div className="cards-container">
+                    {sampleQA.map((item, index) => (
+                        <div className="qa-card" key={index}>
+                            <div className="card-question">
+                                <h3>{item.question}</h3>
+                            </div>
+                            <div className="card-answer">
+                                <p>{item.answer}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default FAQ;
